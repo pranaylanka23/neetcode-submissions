c45b0ec6a409e29e1c62445c611101b177e2fb79
Pranay <pranaylanka@outlook.com>
2026-08-02 17:42:59 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists)==0 || lists[0]==nil { return nil}
+	if len(lists)==1 { return lists[0]} 
+	head := lists[0]
+	for i:=1; i<len(lists);i++{
+		head = merge2Lists(lists[i], head)
+	}
+	return head
+}
+
+func merge2Lists(head1, head2 *ListNode) *ListNode{
+	if head1==nil{ return head2}
+	if head2==nil { return head1}
+	if head1.Val>head2.Val{
+		head2.Next = merge2Lists(head1, head2.Next)
+		return head2
+	}
+	head1.Next = merge2Lists(head1.Next, head2)
+	return head1
+}
